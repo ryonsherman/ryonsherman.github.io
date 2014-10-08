8ab8e47cb10485b404a023e550dab1a57c9585be
Ryon Sherman <ryon.sherman@gmail.com>
2014-10-08 13:01:26 -0500

    Moved ascii_filter() jinja2 filter to project as it is a custom implementation

--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@ from noise import Noise
 
 app = Noise(__name__)
 
+from util import ascii_filter
+app.template.env.filters['ascii'] = ascii_filter
+
 @app.route('/')
 def index(page):
     # set some template variables
